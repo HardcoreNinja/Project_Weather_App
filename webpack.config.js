63a9435263ddf403154dd7079c8b076251a298df
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-24 14:18:58 -0400

    Revert "Implemented the html webpack plugin to dynamically make the index.html file and enabled clean to clean up the dist folder."
    This reverts commit e1a761536869c0a06f9f12e74334329f7e07fdae.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Weather App',
-      template: 'src/index.html'
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
