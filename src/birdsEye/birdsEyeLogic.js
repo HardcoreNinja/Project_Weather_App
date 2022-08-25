27094980e10336d309dc3d1992b282a274408eb6
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-25 17:21:56 -0400

    Created captializeFirstLetter() function for weather description copy.
    Created captializeFirstLetter() function for weather description copy.

--- src/birdsEye/birdsEyeLogic.js
+++ src/birdsEye/birdsEyeLogic.js
@@ -1,7 +1,29 @@
+// eslint-disable-next-line import/no-cycle
 import { metricWeatherObject, imperialWeatherObject } from '../nav/navLogic';
 
-const getMetricWeatherDescription = () => metricWeatherObject.weather[0].description;
-const getImperialWeatherDescription = () => imperialWeatherObject.weather[0].description;
+function capitalizeFirstLetter(string) {
+  console.log(string);
+  const position = string.search(' ');
+  let tempString = '';
+  if (position !== -1) {
+    const stringArray = string.split(' ');
+
+    for (let i = 0; i < stringArray.length; i += 1) {
+      const text = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
+      tempString += `${text} `;
+    }
+  } else {
+    tempString = string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  return tempString;
+}
+const getMetricWeatherDescription = () => capitalizeFirstLetter(
+  metricWeatherObject.weather[0].description,
+);
+const getImperialWeatherDescription = () => capitalizeFirstLetter(
+  imperialWeatherObject.weather[0].description,
+);
 
 const getMetricName = () => metricWeatherObject.name;
 const getImperialName = () => imperialWeatherObject.name;
