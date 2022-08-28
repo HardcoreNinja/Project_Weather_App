ebc6820e780fe36430d7038aab3190b054160f35
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-28 15:58:41 -0400

    Created global media.js and moved all of the background images to it.
    Created global media.js and moved all of the background images to it.

--- src/week/weekLogic.js
+++ src/week/weekLogic.js
@@ -4,25 +4,26 @@ import { latLon, metricWeatherObject, displayFormatWaring } from '../nav/navLogi
 import { createBirdsEye } from '../birdsEye/birdsEyeContent';
 import { createStats } from '../stats/statsContent';
 import { createWeek } from './weekContent';
-
-import d01 from '../resources/images/01d.jpg';
-import n01 from '../resources/images/01n.jpg';
-import d02 from '../resources/images/02d.jpg';
-import n02 from '../resources/images/02n.jpg';
-import d03 from '../resources/images/03d.jpg';
-import n03 from '../resources/images/03n.jpg';
-import d04 from '../resources/images/04d.jpg';
-import n04 from '../resources/images/04n.jpg';
-import d09 from '../resources/images/09d.jpg';
-import n09 from '../resources/images/09n.jpg';
-import d10 from '../resources/images/10d.jpg';
-import n10 from '../resources/images/10n.jpg';
-import d11 from '../resources/images/11d.jpg';
-import n11 from '../resources/images/11n.jpg';
-import d13 from '../resources/images/13d.jpg';
-import n13 from '../resources/images/13n.jpg';
-import d50 from '../resources/images/50d.jpg';
-import n50 from '../resources/images/50n.jpg';
+import {
+  d01,
+  n01,
+  d02,
+  n02,
+  d03,
+  n03,
+  d04,
+  n04,
+  d09,
+  n09,
+  d10,
+  n10,
+  d11,
+  n11,
+  d13,
+  n13,
+  d50,
+  n50,
+} from '../globalCode/media';
 
 let metricForecastObject = {};
 let imperialForecastObject = {};
